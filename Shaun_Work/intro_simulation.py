314d9b4948fca67313c317eaac9cce9df84513ed
Shaun Tan <sonic9889@hotmail.com>
2021-12-09 16:04:07 +0000

    Updated code for changing birthdate to age

--- Shaun_Work/intro_simulation.py
+++ Shaun_Work/intro_simulation.py
@@ -1,10 +1,19 @@
 import random
 from random import sample
+from datetime import date, datetime
+from pydbgen import pydbgen
 
 list = [1,2,3,4,5]
 print(sample(list, 5))
 
-from pydbgen import pydbgen
+def calculate_age(born):
+    born = datetime.strptime(born, '%Y-%m-%d').date()
+    today = date.today()
+    return today.year - born.year - ((today.month,
+                                      today.day) < (born.month,
+                                                    born.day))
 DB = pydbgen.pydb()
 name_date = DB.gen_dataframe(251, fields=['name','date'])
 date = DB.gen_data_series(251, data_type='date')
+name_date['age'] = name_date['date'].apply(calculate_age)
+
